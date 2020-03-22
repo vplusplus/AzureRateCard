075364b95293e3d8f3c0ae466b73f8e50e1f4294
Venkatesh.R <vplusplus@live.com>
2020-03-21 22:34:31 -0400

    Config cleanup

--- src/AzureRatecard/MyConfig.cs
+++ src/AzureRatecard/MyConfig.cs
@@ -1,15 +1,29 @@
 ﻿
+using System;
 
 namespace AzureRateCard
 {
     class MyConfig
     {
-        internal static string MyTenantId => "vplusplus.onmicrosoft.com";
-        internal static string MyClientId => "e4319cec-375f-410c-8116-1808e50b7324";
-        internal static string MyUserId => "ratecardclient@vplusplus.onmicrosoft.com";
-        internal static string MyPassword => "2ABD05AE-95D1-4B6C-848F-0D58323833C2";
+        internal static string MyTenantId => GetAppSettings("RateCard.TenantId");
+        internal static string MyClientId => GetAppSettings("RateCard.ClientId");
+        internal static string MyUserId => GetAppSettings("RateCard.UserId");
+        internal static string MyPassword => GetAppSettings("RateCard.Password");
 
-        //internal static string BillingServiceBaseUrl => "https://management.azure.com";
-        //internal static string MyClientSecret => "/V83dk_vYlqn9ohk[0=[@ovMS-xeo9zW";
+        static string GetAppSettings(string name)
+        {
+            var value = System.Configuration.ConfigurationManager.AppSettings.Get(name);
+            return string.IsNullOrEmpty(value)
+                ? throw new Exception($"AppSettings missing config entry: {name}")
+                : value;
+        }
+
+        static string GetAppSettings(string name, string defaultValue)
+        {
+            var value = System.Configuration.ConfigurationManager.AppSettings.Get(name);
+            return string.IsNullOrEmpty(value)
+                ? defaultValue
+                : value;
+        }
     }
 }
--- src/UnitTests/MyConfig.cs
+++ src/UnitTests/MyConfig.cs
@@ -6,14 +6,6 @@ namespace UnitTests
 {
     class MyConfig
     {
-        internal static string MyTenantId => "vplusplus.onmicrosoft.com";
-        internal static string MyClientId => "e4319cec-375f-410c-8116-1808e50b7324";
-        internal static string BillingServiceBaseUrl => "https://management.azure.com";
-
-
-
-        //internal static string MySubscriptionId => "";
-
 
     }
 }
